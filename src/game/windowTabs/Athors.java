91386209e9d1d3c3d2349ff79ff949dc57718d9b
User <User@ART-PC>
2020-11-07 22:23:54 +0500

    Добавлена кнопка "Play". Изменён шрифт на Times New Roman

--- src/game/windowTabs/Athors.java
+++ src/game/windowTabs/Athors.java
@@ -2,6 +2,7 @@ package game.windowTabs;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import game.Window;
+import java.awt.Font;
 public class Athors extends WindowTab {
 	@Override 
 	public void call(byte r) {	
@@ -9,13 +10,17 @@ public class Athors extends WindowTab {
 	public Athors() {
 		setLayout(null);
 		
+		Font text = new Font("Times New Roman", Font.BOLD, 150);
+		Font butt = new Font("Times New Roman", Font.BOLD, 30);
 		JButton but_exit = new JButton("Back");
-		but_exit.setBounds(Window.resolution.width/2-50, Window.resolution.height/10*9, 100, 50);
+		but_exit.setFont(butt);
+		but_exit.setBounds(Window.resolution.width/2-75, Window.resolution.height/10*9, 150, 50);
 		but_exit.addActionListener(new Put_back());
 		add(but_exit);
 		
 		JLabel athors_text = new JLabel("Art, Колясик");
-		athors_text.setBounds(Window.resolution.width/2-50, Window.resolution.height/2-25, 100, 50);
+		athors_text.setFont(text);
+		athors_text.setBounds(Window.resolution.width/2-420, Window.resolution.height/2-200, 1000, 400);
 		add(athors_text);
 	}
 }
--- src/game/windowTabs/Mini_menu.java
+++ src/game/windowTabs/Mini_menu.java
@@ -1,4 +1,6 @@
 package game.windowTabs;
+import java.awt.Font;
+
 import javax.swing.JButton;
 
 import game.Window;
@@ -9,15 +11,25 @@ public class Mini_menu extends WindowTab {
 	public Mini_menu() {
 		setLayout(null);
 		
-		JButton but_exit = new JButton("Exit");
-		but_exit.setBounds(100, 100, 100, 50);
-		but_exit.addActionListener(new Put_exit());
-		add(but_exit);
+		Font myFont = new Font("Times New Roman", Font.BOLD, 30);
+		
+		JButton exit = new JButton("Exit");
+		exit.setBounds(Window.resolution.width/2-75, Window.resolution.height/2+100, 150, 50);
+		exit.addActionListener(new Put_exit());
+		exit.setFont(myFont);
+		add(exit);
 		
 		JButton athors = new JButton("Athors");
-		athors.setBounds(100, 350, 100, 50);
+		athors.setBounds(Window.resolution.width/2-75, Window.resolution.height/2, 150, 50);
 		athors.addActionListener(new Put_athors());
+		athors.setFont(myFont);
 		add(athors);
+		
+		JButton play = new JButton("Play");
+		play.setBounds(Window.resolution.width/2-75, Window.resolution.height/2-100, 150, 50);
+		play.addActionListener(new Put_play());
+		play.setFont(myFont);
+		add(play);
 	}
 }
 class Put_exit implements java.awt.event.ActionListener {
@@ -30,5 +42,12 @@ class Put_athors implements java.awt.event.ActionListener {
 	@Override 
 	public void actionPerformed(java.awt.event.ActionEvent e) {
 		Window.self.switchTab(new Athors());
+	}
+}
+class Put_play implements java.awt.event.ActionListener {
+	@Override 
+	public void actionPerformed(java.awt.event.ActionEvent e) {
+		System.exit(0);
+//		Window.self.switchTab(’ƒ≈ »√–¿ ?????????????);
 	}
 }
