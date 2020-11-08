97447c8aa36145aea64bf6f98420df959f354cee
ArtificialAuthor <73090035+ArtificialAuthor@users.noreply.github.com>
2020-11-08 22:31:16 +0500

    Добавлена система GameObject и связанная с ней Behavivour

---
+++ src/game/objects/Behavivour.java
@@ -0,0 +1,9 @@
+//Код Artificial
+//Зависимости
+package game.objects;
+
+//Класс - поведение объекта
+public abstract class Behavivour {
+	//CFG 1
+	public String ID;
+}
---
+++ src/game/objects/GameObject.java
@@ -0,0 +1,42 @@
+//Код Artificial
+//Зависимости
+package game.objects;
+import java.util.ArrayList;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+//Класс - игровой объект
+public class GameObject extends javax.swing.JPanel{
+	//CFG 1
+	public String name = "GameObject";
+	public double position[] = {0, 0};
+	public double size[] = {0, 0};
+	ArrayList<Behavivour> behavivour = new ArrayList<Behavivour>();
+	
+	//CFG 2
+	//Конструктор без имени
+	public GameObject() {
+		
+	}
+	//Конструктор с именем
+	public GameObject(String setName) {
+		name = setName;
+	}
+	
+	//CFG 3
+	//Обрисовка
+	@Override
+	public void paintComponent(Graphics g) {
+		Graphics2D g2 = (Graphics2D) g;
+		g2.fillRect(0, 0, getWidth(), getHeight());
+	}
+	//Выдергивание компонента
+	public Behavivour getBehavivour(String behavivourID) throws Exception {
+		for (Behavivour beh : behavivour) {
+			if(beh.ID == behavivourID) {
+				return beh;
+			}
+		}
+		throw new Exception("У "+name+" нет game.objects.behavivours."+behavivourID+"!");
+	}
+}
